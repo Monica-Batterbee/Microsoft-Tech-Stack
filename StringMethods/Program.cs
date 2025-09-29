25430b345781a14dc06257b3562606c2e0d678e8
monica-mae <monica.mae123@outlook.com>
2025-09-29 11:17:48 +0100

    Add project files.

---
+++ StringMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.InteropServices;
+
+static string ConcatString(string str1, string str2)
+{
+    string result = string.Concat(str1, " ", str2);
+    Console.WriteLine(result);
+
+    return result;
+}
+
+static string ReplaceString(string str1, string replace, string replaceWith)
+{
+    string result = str1.Replace(replace, replaceWith);
+    Console.WriteLine(result);
+
+    return result;
+}
+
+
+static string UpperString(string str)
+{
+    string result = str.ToUpper();
+    Console.WriteLine(result);
+
+    return result;
+}
+
+string concatResult = ConcatString("Hello", "World!");
+string replaceResult = ReplaceString(concatResult, "World!", "User!");
+string upperResult = UpperString(replaceResult);
+
